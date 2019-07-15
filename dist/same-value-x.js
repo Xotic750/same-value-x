91388243d8c25c6485d32b9a663a131ee9cb8a6a
Graham Fairweather <xotic750@gmail.com>
2019-07-15 12:31:20 +0200

    :bookmark: v2.0.8
    Auto build

--- dist/same-value-x.js
+++ dist/same-value-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-14T21:35:23.472Z",
+  "date": "2019-07-15T10:31:16.656Z",
   "describe": "",
   "description": "Determines whether two values are the same value.",
   "file": "same-value-x.js",
   "hash": "7472724c727e9386d84b",
   "license": "MIT",
-  "version": "2.0.7"
+  "version": "2.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
